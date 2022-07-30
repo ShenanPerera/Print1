c88dcd027eb0e683bb03fab4a910573306fb8ba9
shenan2001 <85660117+ShenanPerera@users.noreply.github.com>
2022-07-30 06:31:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+  public static void main(String arg[]){
+    System.out.print("This is line 1");
+    System.out.print(" still line 1");
+    System.out.println(" lets move to the next line");
+    System.out.println("finally line 2");
+  }
+}
